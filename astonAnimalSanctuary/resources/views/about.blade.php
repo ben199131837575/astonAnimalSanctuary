78975d50b609ca4401f664f9fff823af0498a3f6
ben <ben199131837575432@gmail.com>
2018-08-07 12:32:50 +0100

    updated iframe code

--- astonAnimalSanctuary/resources/views/about.blade.php
+++ astonAnimalSanctuary/resources/views/about.blade.php
@@ -8,7 +8,7 @@
             <div class="panel panel-primary">
                     <div class="panel-heading">
                         <div class="embed-responsive embed-responsive-16by9">
-                            <iframe width="794" height="447" src="https://www.youtube.com/embed/83IHKMMnWjc?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <iframe width="794" height="447" src="https://www.youtube.com/embed/sVZOfZ8eaYg?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
@@ -16,6 +16,4 @@
         </div>
     </div>
 </div>
-
-
 @endsection
--- astonAnimalSanctuary/storage/framework/views/0785d77ef595d3f7c037d3a54f4cb33d54a174f0.php
+++ astonAnimalSanctuary/storage/framework/views/0785d77ef595d3f7c037d3a54f4cb33d54a174f0.php
@@ -6,7 +6,7 @@
             <div class="panel panel-primary">
                     <div class="panel-heading">
                         <div class="embed-responsive embed-responsive-16by9">
-                            <iframe width="794" height="447" src="https://www.youtube.com/embed/83IHKMMnWjc?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <iframe width="794" height="447" src="https://www.youtube.com/embed/sVZOfZ8eaYg?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
@@ -14,8 +14,6 @@
         </div>
     </div>
 </div>
-
-
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
